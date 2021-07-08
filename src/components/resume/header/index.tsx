ba6fe92e56c0d5a90917750d8c3b88e8110b4d68
Maciej Lewicki <lewickimapl@gmail.com>
2021-07-09 00:35:54 +0200

    create liquid template for header

--- src/components/resume/header/index.tsx
+++ src/components/resume/header/index.tsx
@@ -1,5 +1,8 @@
 
+import { ReactLiquid } from 'react-liquid';
+
 import './style.scss';
+import template from './template.js';
 
 type Header = {
   name: string;
@@ -10,36 +13,9 @@ type Header = {
   info: string[];
 }
 
-const ResumeHeader = ({
-  name, profile, position, separator, info
-}: Header) => {
+const ResumeHeader = (header: Header) => {
   return (
-    <div className='resume-header' style={{borderBottom: separator ? '1px solid black' : undefined}}>
-      <img
-        src={profile}
-        className='resume-photo'
-        alt='Profile'
-      />
-      <div className='resume-info'>
-        <div className='resume-info-main'>
-            <h1 className='resume-header-name'>{name}</h1>
-            <h2 className='resume-header-position'>{position}</h2>
-        </div>
-        <p className='resume-header-links'>
-          {info.map((item, index, array) => {
-            if(item === 'br') {
-              return (<br />);
-            }
-
-            if(index + 1 === array.length) {
-              return (<> {item} </>);
-            }
-
-            return <> {item} &bull; </>
-          })}
-        </p>
-      </div>
-    </div>
+    <ReactLiquid template={template} data={header} html />
   );
 };
 
---
+++ src/components/resume/header/template.js
@@ -0,0 +1,22 @@
+
+// using a javascript file as a temporary measure bc im too tired rn
+// to figure out webpack
+export default `
+<div class='resume-header'>
+  <img src='{{ profile }}' class='resume-photo' alt='Profile' />
+  <div class='resume-info'>
+    <div class='resume-info-main'>
+      <h1 class='resume-header-name'>{{ name }}</h1>
+      <h2 class='resume-header-position'>{{ position }}</h2>
+    </div>
+    <p class='resume-header-links'>
+      {% for item in info %}
+        {% if item == 'br' %}
+          <br />
+        {% else %}
+          {{ item }} &bull;
+        {% endif %}
+      {% endfor %}
+    </p>
+  </div>
+</div>`;
